874bab79aa7a9c6fefe56d8544c962193330f5cb
sdevagup <dsspravallika@gmail.com>
2025-10-04 19:39:27 +0000

    set migrations to true

--- MovieReview.Data/Migrations/Configuration.cs
+++ MovieReview.Data/Migrations/Configuration.cs
@@ -2,7 +2,7 @@ namespace MovieReview.Data.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MovieReviewDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<MovieReviewDbContext>
     {
         public Configuration()
         {
